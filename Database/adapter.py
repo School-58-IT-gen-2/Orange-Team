037fd46cb11e2b97bb898cf2f8fd9a1accc23497
Alexey Pisanenko <alexpisanenko2008@mail.ru>
2024-03-07 21:59:04 +0300

    Добавление методов в адаптер

--- Database/adapter.py
+++ Database/adapter.py
@@ -38,26 +38,6 @@ class AdapterDB:
         data = cursor.fetchall()
         return data
     
-        '''def get_smth(self, column: str, table_name: str):
-            request = f'SELECT {column} FROM "Galactic Empire"."{table_name}"'
-            cursor = self.conn.cursor()
-            cursor.execute(request)
-            data = cursor.fetchall()
-            return data
-        
-        def order_by(self, selected_data: str, order_type: str):
-            request = f'{selected_data} ORDER BY '
-            cursor = self.conn.cursor()
-            cursor.execute(request)
-            data = cursor.fetchall()
-            return data
-        
-        def where(self, condition: str):
-            request = f'{selected_data} WHERE condition'
-            cursor = self.conn.cursor()
-            cursor.execute(request)
-            data = cursor.fetchall()
-            return data'''
     def get_by_sql(self, selection: str):
         request = f'{selection}'
         cursor = self.conn.cursor()
@@ -65,10 +45,48 @@ class AdapterDB:
         data = cursor.fetchall()
         return data
     
+    def update_by_id(self, table_name: str, update: str, id: int):
+        update_param = update.split('=')[0]
+        update_value = update.split('=')[1]
+        request = f'UPDATE "Galactic Empire"."{table_name}" SET "{update_param}"=\'{update_value}\' WHERE id = {id}'
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        self.conn.commit()
+        request = f'SELECT * FROM "Galactic Empire"."{table_name}"'
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        data = cursor.fetchall()
+        return data
+    
+    def delete_by_id(self, table_name: str, id: int):
+        request = f'DELETE FROM "Galactic Empire"."{table_name}" WHERE id = {id}'
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        self.conn.commit()
+        request = f'SELECT * FROM "Galactic Empire"."{table_name}" '
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        data = cursor.fetchall()
+        return data
+    
+    def insert(self, table_name: str, insert_data: list):
+        columns = ''
+        values = ''
+        for i in insert_data:
+            columns += f'"{i.split('=')[0]}", '
+            values += f"'{i.split('=')[1]}', "
+        columns = columns[:-2]
+        values = values[:-2]
+        request = f'INSERT INTO "Galactic Empire"."{table_name}" ({columns}) VALUES ({values})'
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        self.conn.commit()
+        request = f'SELECT * FROM "Galactic Empire"."{table_name}" '
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        data = cursor.fetchall()
+        return data
+    
 a = AdapterDB()
 
-print(a.get_by_sql('SELECT * FROM "Galactic Empire"."Cruisers"'))
-
-print(a.get_by_sql('SELECT "Name", "Star Type" FROM "Galactic Empire"."Systems" WHERE "Systems"."Allegiance"=\'Empire\' ORDER BY id ASC'))
-
-print(a.get_by_sql('SELECT "Name", "Population" FROM "Galactic Empire"."Planets" WHERE "Planets"."Name"!=\'mityas planet\' AND "Planets"."Population">50 and "Name"=lower("Name") ORDER BY "Position" ASC'))
+print(a.insert('Systems', ['Name=John', 'Star Type=black hole', 'Allegiance=Empire']))
